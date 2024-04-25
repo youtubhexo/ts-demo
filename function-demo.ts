fba9d38dc4df5b04960fae82f6ef5dc022caad7b
张王岩 <youtubhexo@gmail.com>
2024-04-25 19:01:08 +0800

    函数重载：
    函数重载：
         在一些强类型语言中（java,c++,C#）,函数重载是指定义几个函数名相同，
    但参数个数不同的函数，在调用时传入不同的参数，编译器会自动调用适合
    的函数。
    
    TS函数重载 区别于其他语言中的重载，TypeScript中的重载是为了针对
    不同参数个数和类型，推断返回值类型。

--- function-demo.ts
+++ function-demo.ts
@@ -9,4 +9,15 @@ const add1 = (arg1: number, arg2: number): number => {
   return arg1 + arg2;
 };
 
-const add2:()
+function handleData(x: string): string[];
+function handleData(x: number): string;
+
+function handleData(x: any) {
+  if (typeof x === 'string') {
+    return x.split('');
+  } else {
+    return '' + x;
+  }
+}
+
+handleData(100).length;
