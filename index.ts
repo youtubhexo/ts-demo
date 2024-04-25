e9a489acfa884354f3f53582f58c63de89e3990a
张王岩 <youtubhexo@gmail.com>
2024-04-25 18:22:10 +0800

    基础类型&引用类型

---
+++ index.ts
@@ -0,0 +1,23 @@
+//基础类型
+
+//string number boolean  symbol  null  undefined
+
+//对象类型{} or object,  [] or Array,  function,Class类型
+
+const msg: string = 'zwy-hello';
+
+let msg1: string = 'AI赋能开发';
+
+const msgStr = `${msg} ${msg1}`;
+console.log(msg);
+
+const flag: boolean = true;
+
+const obj: {} = {};
+
+const obj2: { msg: string; id?: number } = {
+  msg: 'zwy',
+  id: 123,
+};
+
+const arr: string[] = ['1'];
