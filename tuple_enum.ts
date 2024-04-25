06d5df6b3abb04306610867e36251fe91cb3b9d4
张王岩 <youtubhexo@gmail.com>
2024-04-25 19:20:44 +0800

    tuple&enum示例

---
+++ tuple_enum.ts
@@ -0,0 +1,11 @@
+//tuple ->元组    固定类型+长度的数组
+
+const teacherInfo: [string, string, number] = ['despeardo', 'youtubhexo', 1];
+//enum-> 枚举
+
+enum Direction {
+  UP = 200,
+  DOWN = 300,
+  LEFT = 400,
+  RIGHT = 500,
+}
