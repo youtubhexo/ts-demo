69a7477de9954a0b086c541c44fbff2e4246b561
张王岩 <youtubhexo@gmail.com>
2024-04-25 18:39:11 +0800

    函数类型

---
+++ function-demo.ts
@@ -0,0 +1,12 @@
+//基本用法
+function add(arg1: number, arg2: number): number {
+  return arg1 + arg2;
+}
+
+//add
+
+const add1 = (arg1: number, arg2: number): number => {
+  return arg1 + arg2;
+};
+
+const add2:()
